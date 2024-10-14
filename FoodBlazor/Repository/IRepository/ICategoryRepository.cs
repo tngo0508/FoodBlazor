4ac74dcaae70c1c49a64310c8ba028c46d7b426c
Thomas Ngo <tngo0508@gmail.com>
2024-10-13 19:54:45 -0700

    Add ICategoryRepository interface
    Introduced ICategoryRepository interface for CRUD operations on Category entities. Removed the unnecessary IRepository folder declaration from the project file.

---
+++ FoodBlazor/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using FoodBlazor.Data;
+
+namespace FoodBlazor.Repository.IRepository;
+
+public interface ICategoryRepository
+{
+    public Category Create(Category category);
+    public Category Update(Category category);
+    public bool Delete(int id);
+    public Category Get(int id);
+    public IEnumerable<Category> GetAll();
+}
