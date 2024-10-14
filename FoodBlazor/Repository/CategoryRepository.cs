27dc4f039a2b4e5d075583d2aa13019c22d8c8c1
Thomas Ngo <tngo0508@gmail.com>
2024-10-13 20:08:02 -0700

    Add CategoryRepository implementation
    Introduce the CategoryRepository class, implementing Create, Update, Delete, Get, and GetAll methods for managing categories within the application database. This addition enables CRUD operations for the Category entity, improving data handling and interaction.

---
+++ FoodBlazor/Repository/CategoryRepository.cs
@@ -0,0 +1,49 @@
+﻿using FoodBlazor.Data;
+using FoodBlazor.Repository.IRepository;
+
+namespace FoodBlazor.Repository;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly ApplicationDbContext _db;
+
+    public CategoryRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    public Category Create(Category category)
+    {
+        _db.Categories.Add(category);
+        _db.SaveChanges();
+        return category;
+    }
+
+    public Category Update(Category category)
+    {
+        var objFromDb = _db.Categories.FirstOrDefault(u => u.Id == category.Id);
+        if (objFromDb == null) return new Category();
+        objFromDb.Name = category.Name;
+        _db.Categories.Update(objFromDb);
+        _db.SaveChanges();
+        return objFromDb;
+    }
+
+    public bool Delete(int id)
+    {
+        var obj = _db.Categories.FirstOrDefault(x => x.Id == id);
+        if (obj == null) return false;
+        _db.Categories.Remove(obj);
+        return (_db.SaveChanges() > 0);
+    }
+
+    public Category Get(int id)
+    {
+        var obj = _db.Categories.FirstOrDefault(x => x.Id == id);
+        return obj ?? new Category();
+    }
+
+    public IEnumerable<Category> GetAll()
+    {
+        return _db.Categories.ToList();
+    }
+}
